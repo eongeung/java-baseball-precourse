42df6c776effac175c1dfd98708adccbd83feec2
eongeung <eongeung1022@gmail.com>
2025-02-18 18:44:16 +0900

    feat : InputView 로직 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,6 +3,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 
 public class InputView {
-    //세자리 숫자 입력
-    //1번 2번 입력하는거
+    public static String UserInput() {
+        return Console.readLine();
+    }
 }
