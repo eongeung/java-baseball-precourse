a3f4c944c1fd30736b6222b3b5c6a06fa39c607f
eongeung <eongeung1022@gmail.com>
2025-02-18 20:01:43 +0900

    feat : UserBall 로직 추가, 예외 처리

--- src/main/java/baseball/model/UserBall.java
+++ src/main/java/baseball/model/UserBall.java
@@ -2,6 +2,16 @@ package baseball.model;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserBall {
-    //예외처리1(세자리 숫자 입력X)
-    //예외처리2(중복숫자입력)
+    public void validateInput(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("세 자리 숫자만 입력해주세요!");
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < 3; j++) {
+                if (input.charAt(i) == input.charAt(j)) { // 같은 숫자가 있으면 중복!
+                    throw new IllegalArgumentException("중복되지 않는 숫자를 입력해주세요!");
+                }
+            }
+        }
+    }
 }
