f4a412f696bd5b0802cbc3604b1e890cf1ccbf0e
eongeung <eongeung1022@gmail.com>
2025-02-18 18:38:46 +0900

    feat : OutputView 로직 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,29 @@
 package baseball.view;
 
 public class OutputView {
-    //세자리 숫자입력
-    //세자리 숫자만 입력하세요
-    //중복숫자입력하지마세요
+    public static void enter(){
+        System.out.println("세 자리 숫자를 입력하세요");
+    }
+    public static void onlyThreeNumber() {
+        System.out.println("세 자리 숫자만 입력해주세요");
+    }
+    public static void duplication() {
+        System.out.println("중복되지 않게 입력해주세요");
+    }
+    public static void restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 눌러주세요");
+    }
+
+    public static void allStrike {
+        System.out.println(strike + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+    public static void strike {
+        System.out.println(strike + "스트라이크");
+    }
+    public static void ball {
+        System.out.println(ball + "볼");
+    }
+    public static void nothing {
+        System.out.println("낫싱");
+    }
 }
