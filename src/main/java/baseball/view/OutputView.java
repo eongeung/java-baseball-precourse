69d2f433e6e8f27118d99270e538d245a69f1522
eongeung <eongeung1022@gmail.com>
2025-02-18 20:13:06 +0900

    feat : OutputView 예외처리 삭제

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,26 +4,20 @@ public class OutputView {
     public static void enter(){
         System.out.println("세 자리 숫자를 입력하세요");
     }
-    public static void onlyThreeNumber() {
-        System.out.println("세 자리 숫자만 입력해주세요");
-    }
-    public static void duplication() {
-        System.out.println("중복되지 않게 입력해주세요");
-    }
     public static void restart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 눌러주세요");
     }
 
-    public static void allStrike {
+    public static void allStrike(int strike) {
         System.out.println(strike + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
-    public static void strike {
+    public static void checkStrike(int strike) {
         System.out.println(strike + "스트라이크");
     }
-    public static void ball {
+    public static void checkBall(int ball) {
         System.out.println(ball + "볼");
     }
-    public static void nothing {
+    public static void nothing() {
         System.out.println("낫싱");
     }
 }
